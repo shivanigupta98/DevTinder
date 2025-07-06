5c0b398820d316e3d9d3d18b9e1d8f0459c90af1
shivanigupta98 <122546563+shivanigupta98@users.noreply.github.com>
2025-07-06 19:36:56 +0530

    Routing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 
 const app = express();
-app.use('/test',(req,res)=>{
-    res.send("Hello from the server!");
+app.get('/users',(req,res)=>{
+    res.send({
+        firstName:"shivani",
+        lastname:"gupta",
+        age:"27"
+    });
+})
+app.post('/users',(req,res)=>{
+    console.log("Saved in database");
+    res.send("Data has been saved successfully!");
+})
+app.delete('/users',(req,res)=>{
+    console.log("Deleted the users data");
+    res.send("Deleted successfully");
 })
 app.listen('3000', ()=>{
     console.log("Server is succesfully listening on port 3000");

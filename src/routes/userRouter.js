5695a1166ab30831086eaf7beb22f992f384a71a
shivanigupta98 <shivani14y@gmail.com>
2025-08-03 22:37:46 +0530

    fedd api & pagination

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,7 +2,9 @@ const express = require('express');
 const userRouter = express.Router();
 const { userAuth } = require('../middlewares/userAuth');
 const ConnectionRequest = require('../models/connectionRequest');
-const authRouter = require('./authRouter');
+const User = require('../models/user');
+const mongoose = require('mongoose');
+
 
 const USER_INFO = "firstName lastName age skills gender about";
 userRouter.get('/user/request/received', userAuth, async (req, res) => {
@@ -31,7 +33,7 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
                 { fromUserId: loggedInUser._id, status: "accepted" }
             ]
         }).populate("fromUserId", USER_INFO).populate("toUserId", USER_INFO);
-        
+
         const data = connectionRequests.map((connection) => {
             if (connection.toUserId._id.toString() === loggedInUser._id.toString()) {
                 return connection.fromUserId;
@@ -47,5 +49,35 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 
     }
 })
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [{ toUserId: loggedInUser._id },
+            { fromUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId");
 
+        let hideConnectionsonFeed = new Set();
+        connectionRequests.map((user) => {
+            hideConnectionsonFeed.add(user.toUserId.toString());
+            hideConnectionsonFeed.add(user.fromUserId.toString());
+        })
+        const feed = await User.find({
+            $and: [{ _id: { $nin: Array.from(hideConnectionsonFeed) } },
+            { _id: { $ne: loggedInUser._id } }]
+        }).select(USER_INFO).skip(skip).limit(limit);
+
+        res.json({ feed });
+
+    }
+    catch (err) {
+        res.status(400).send("Error: " + err.message);
+    }
+
+})
 module.exports = userRouter;

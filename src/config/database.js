9abad6ae752e3383adaebe9d9d8f9ae53cf57f04
shivanigupta98 <shivani14y@gmail.com>
2025-09-09 20:57:27 +0530

    updated db changes

--- src/config/database.js
+++ src/config/database.js
@@ -3,16 +3,8 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
-  if (!MONGO_URI) {
-    throw new Error("MONGO_URI not defined in environment");
-  }
-
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: true,
-    });
+    await mongoose.connect(MONGO_URI); 
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("Database cannot be connected:", err);

8d6e527c21d2948193d36d32132ba1adc0cb5a4d
shivanigupta98 <shivani14y@gmail.com>
2025-09-09 22:05:40 +0530

    added frontend url from env

--- src/app.js
+++ src/app.js
@@ -16,9 +16,10 @@ const userRouter = require("./routes/userRouter");
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin: FRONTEND_URL,
   credentials:true
 }
 ));
